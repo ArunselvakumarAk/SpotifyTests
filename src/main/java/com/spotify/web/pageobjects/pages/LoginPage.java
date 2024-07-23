4b05dd35e3f8ec801c1a3af4d459a0bc9805b35b
ArunselvakumarAK <arunselvakumarak@gmail.com>
2024-07-23 23:11:40 +0530

    minor changes in page objects

--- src/main/java/com/spotify/web/pageobjects/pages/LoginPage.java
+++ src/main/java/com/spotify/web/pageobjects/pages/LoginPage.java
@@ -1,6 +1,8 @@
 package com.spotify.web.pageobjects.pages;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
 
 import com.spotify.web.pageobjects.base.BasePage;
@@ -12,6 +14,8 @@ public class LoginPage extends BasePage{
 	private By password_txt = By.id("login-password");
 	private By logInBtn = By.xpath("//span[text()='Log In']");
 	private By web_player_btn = By.xpath("//span[text()='Web Player']");
+	private By verifyCaptcha_chkbox = By.xpath("//div[@class='recaptcha-checkbox-border']");
+	private By confirmCaptcha_btn = By.xpath("//span[text()='Continue']");
 
 	public LoginPage(WebDriver driver) {
 		super(driver);
@@ -33,20 +37,36 @@ public class LoginPage extends BasePage{
 	}
 	
 	public LoginPage clickWebplayerBtn() {
-		driver.findElement(web_player_btn).click();
+		try {
+			utils.waitForVisibilityOfElement(web_player_btn);
+			driver.findElement(web_player_btn).click();
+		}catch(NoSuchElementException | TimeoutException e) {
+			ResourceUtils.log.info("Found captcha");
+			clickVerifyCaptchaChkbox();
+			clickConfirmCaptchaBtn();
+			ResourceUtils.log.info("Captcha handled");
+		}
 		return this;
 	}
 	
 	public LoginPage handleLogin(String username, String password) {
 		utils.srollIntoView(driver.findElement(logInBtn));
 		enterUsername(username).enterPassword(password).clickLoginBtn();
-		utils.waitForVisibilityOfElement(web_player_btn);
 		clickWebplayerBtn();
 		return this;
 	}
 	
 	public LoginPage navigateToLoginPage() {
 		driver.get(ResourceUtils.getProperty("configuration//web_config.properties", "accountsUrl"));
+		return this;
+	}
+		
+	private LoginPage clickVerifyCaptchaChkbox() {
+		driver.findElement(verifyCaptcha_chkbox).click();
+		return this;
+	}
+	private LoginPage clickConfirmCaptchaBtn() {
+		driver.findElement(confirmCaptcha_btn).click();
 		return this;
 	}
 }
